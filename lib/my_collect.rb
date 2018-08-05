d9d6a6f84adddc59e69c384932e9bd8f14592bc8
Brian DiRito <bdirito@gmail.com>
2018-08-05 13:35:44 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,25 @@
+def my_collect(array)
+  new_collection = []
+  counter = 0
 
+  if array.length == 0
+    return array
+  end
 
+  if array.include?("ruby")
+  while counter < array.length
+      yield array[counter]
+      new_collection << array[counter].upcase
+      counter += 1
+    end
+  end
+
+  if array.include?('Tim Jones')
+  while counter < array.length
+      yield array[counter]
+      new_collection << array[counter].split(" ").first
+      counter += 1
+    end
+  end
+  new_collection
+end
