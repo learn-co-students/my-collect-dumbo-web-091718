df0c5d02a8732835cca88f01eff86c55abd362c1
Cristina Murillo <cmurill1@jhu.edu>
2018-08-16 17:36:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+ new_array = [] 
+ if block_given?
+    i = 0 
+    while i < array.size 
+     new_array.push(yield array[i])
+      i+=1 
+    end
+  end
+  new_array 
+end
 
