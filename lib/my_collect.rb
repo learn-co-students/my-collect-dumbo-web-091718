e016fcdd8110ac21117d74a429b5aeb112206c49
Jason Odoom <jasonodoom@gmail.com>
2018-09-04 00:25:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect (empty_array)
+  i = 0
+  collect_array = []
+  while i < empty_array.length do  
+  collect_array << yield(empty_array[i])
+  i= i+1
+  end   
+  collect_array
+end
 
