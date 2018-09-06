e89d99d5d29e7048fd62214eb31e9c3196ce84cc
christine gomez <christine.gomez1226@gmail.com>
2018-09-06 20:04:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(array)
+  new_collect=[]
+  i = 0
+  while i < array.length
+  new_collect.push yield(array[i])
+  i+=1
+  end
+  new_collect
+end
